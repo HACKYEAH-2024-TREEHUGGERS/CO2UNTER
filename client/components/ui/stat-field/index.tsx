1318cdea0c1db7dadadcc201c7a4137207b97163
Karol Jażdrzyk <karol1404@vp.pl>
2024-09-29 00:26:33 +0200

    Add StatField component

---
+++ client/components/ui/stat-field/index.tsx
@@ -0,0 +1,70 @@
+import { Colors } from '@/constants/Colors';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {
+  icon:
+    | 'nature'
+    | 'nature-people'
+    | 'directions-car'
+    | 'directions-bus'
+    | 'directions-bike';
+  value: string;
+  label: string;
+  valueTextSize?: 'big' | 'small';
+};
+
+export const StatField = ({
+  icon,
+  value,
+  label,
+  valueTextSize = 'big',
+}: Props) => {
+  const isBigValue = valueTextSize === 'big';
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.valueContainer}>
+        <MaterialIcons
+          name={icon}
+          size={24}
+          color={Colors.light.neutral[700]}
+        />
+        <Text
+          style={{
+            fontSize: isBigValue ? 24 : 14,
+            fontWeight: isBigValue ? 'bold' : 'medium',
+          }}
+        >
+          {value}
+        </Text>
+      </View>
+      <Text style={styles.label}>{label}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: Colors.light.neutral[50],
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  value: {
+    color: Colors.light.neutral[900],
+  },
+  label: {
+    fontSize: 14,
+    color: Colors.light.neutral[900],
+  },
+});
