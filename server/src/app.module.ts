ffc3c655546b9e439e6df801dc3392058d92dfef
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-09-28 19:59:35 +0200

    feat(backend): database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT, 10),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      entities: [],
+      synchronize: false,
+      ssl: process.env.SSL_CERT_DAYS ? { rejectUnauthorized: false } : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
