3bc52933f1ef08c4b94942d15815d503cfd1e415
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2024-09-29 11:18:07 +0200

    feat(client): add calculator placeholder

--- client/app/(app)/(homeScreenTabs)/calculator.tsx
+++ client/app/(app)/(homeScreenTabs)/calculator.tsx
@@ -1,9 +1,27 @@
+import { Colors } from '@/constants/Colors';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function CalculatorTab() {
   return (
     <View style={styles.container}>
-      <Text>Calculator</Text>
+      <Text
+        style={{
+          fontSize: 16,
+          fontWeight: '500',
+          color: Colors.light.neutral[900],
+        }}
+      >
+        Kalkulator Rocznej Absorpcji
+      </Text>
+      <Text
+        style={{
+          marginTop: 'auto',
+          marginBottom: 'auto',
+          color: Colors.light.neutral[700],
+        }}
+      >
+        Wkrótce dostępny!
+      </Text>
     </View>
   );
 }
@@ -11,5 +29,7 @@ export default function CalculatorTab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
   },
 });
